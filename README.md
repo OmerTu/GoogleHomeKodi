c75cc3fbc236ef94c997cd7350e4b7b73f9eb087
System <none>
2017-05-09 03:09:36 +0000

    🏦👆 Updated with Glitch

--- README.md
+++ README.md
@@ -1,27 +1,95 @@
-Welcome to the Glitch BETA
+Control Kodi through your Google Home / Google Assistance
 =========================
 
-Click `Show` in the header to see your app live. Updates to your code will instantly deploy and update live.
-
-Glitch is a developer playground that lets you code a real web-app without the slow setup and deployment steps.
-
-[About Glitch](https://glitch.com/about)
-
-
-Your Project
 ------------
+## What it can do
 
-On the back-end,
-- your app starts at `server.js`
-- add frameworks and packages in `package.json`
-- safely store app secrets in `.env`
+Eventually, this let you to easily control your kodi using simple voice commands.
 
-On the front-end,
-- edit `public/client.js`, `public/style.css` and `views/index.html`
-- drag in `assets`, like images or music, to add them to your project
+### **Play a movie:**
+"Hey Google, kodi play movie [movie name]" --> will search for the given movie name and play it.
+
+### **Play next unwatched episode:**
+"Hey Google, kodi play tv show [tv show name]" --> will search for the given tv show and play the next unwatched episode.
+
+### **Pause / Resume kodi:**
+"Hey Google, pause kodi"
+
+### **Stop kodi:**
+"Hey Google, stop kodi"
+
+------------
+## How to setup
+
+Disclaimer: The security aspect of this setup can be improved. Use on your own risk and choose complex username & password in the below steps.
+
+### **A) Enable webserver access in kodi**
+1. In Kodi, go to *Settings* >> *Web server*
+2. Set *Allow remote contorl via HTTP* to On
+3. Choose a port number (e.g. 8080)
+4. Choose a username and password (Important!)
+5. Configure your router to forward the port your selected to your kodi device
+6. Find your external IP address (i.e. Google 'what's my ip?')
 
 
-Made by Fog Creek
+### **B) Setup a Google assistance to kodi webserver in Glitch**
+1. Go to [Glitch.com](https://glitch.com) and sign in with your github user
+2. Create a new Glitch project and under *advance settings* choose *Import from GitHub*
+3. Enter this project *OmerTu/GoogleHomeKodi*
+4. Change Glitch project settings to private (under *advance settings*)
+5. Edit the *.env* file in your Glitch project with the following settings:
+```
+KODI_IP="[YOUR_EXTERNAL_IP_ADDRESS]"
+KODI_PORT="[YOUR_KODI_PORT]"
+KODI_USER="[YOUR_KODI_USER_NAME]"
+KODI_PASSWORD="[YOUR_KODI_PASSWORD]"
+```
+6. Check your Glitch server address by choosing 'Show Live' on the top left. A new tab with your server will open. Note your server address in the address bar.
+
+
+### C) Set up IFTTT with your Google Home
+1. Go to [IFTTT](https://ifttt.com)
+2. Create a new applet: if *This* then *That*
+  1. For *This* choose: *Google Assistance*
+  2. Choose *Say a phrase with a text ingredient*
+  3. In *What do you want to say?* enter something like:
+  > Kodi play movie $
+  
+  4. In *What do you want the Assistant to say in response?* enter something like:
+  > ok playing $ movie
+  
+  5. For *That* choose: *Maker Webhooks*
+  6. Choose *Make a web request*
+  7. In *URL* enter:
+  >[YOUR_GLITCH_SERVER_ADDRESS]/playmovie?q= {{TextField}}
+  
+  For example, if your glitch server address is 'green-icecream.glitch.me', your should enter:
+  >https://green-icecream.glitch.me/playmovie?q= {{TextField}}
+  
+  8. Method: *Get*
+  9. Content Type: *application/json*
+
+Now every time you say "Hey Google, Kodi play movie bla bla", it should play bla bla on your kodi.
+
+**Note1:** If your external IP changes, this will stop working (consider getting a static IP address)
+
+**Note2:** Eventually anyone who'll send a get request to your glitch server will be able to play a movie or stop your kodi player. (a better security mechanism could fix this).
+
+For Tv show support, follow all the steps in **C**, except choose a different phrase (e.g. "Kodi play an episode of $") and use this URL:
+>[YOUR_GLITCH_SERVER_ADDRESS]/playtvshow?q= {{TextField}}
+
+To pause or resume kodi follow the instructions in **C** but choose *Say a simple phrase* in step 2 and use this URL:
+>[YOUR_GLITCH_SERVER_ADDRESS]/playpause
+
+------------
+## Credits ##
+I used some code from these 2 projects:
+https://github.com/MarvinSchenkel/HomeyKodi
+https://github.com/Jephuff/kodi-rpc
+
+And this wonderful website makes setting this up super easy -  Glitch (https://glitch.com/about)
+
+
 -----------------
-
-\ ゜o゜)ノ
+Made by Omer Turgeman
+-----------------
--- server.js
+++ server.js
@@ -10,6 +10,18 @@ var Fuse = require('fuse.js')
 var Kodi = require('./kodi-connection/node.js');
 var kodi = new Kodi(process.env.KODI_IP, process.env.KODI_PORT, process.env.KODI_USER, process.env.KODI_PASSWORD);
 
+// Set option for fuzzy search
+var fuzzySearchOptions = {
+  caseSensitive: false, // Don't care about case whenever we're searching titles by speech
+  includeScore: false, // Don't need the score, the first item has the highest probability
+  shouldSort: true, // Should be true, since we want result[0] to be the item with the highest probability
+  threshold: 0.4, // 0 = perfect match, 1 = match all..
+  location: 0,
+  distance: 100,
+  maxPatternLength: 64,
+  keys: ['label']
+}
+
 app.use(express.static('public'));
 
 app.get("/", function (request, response) {
@@ -24,7 +36,15 @@ app.get("/playpause", function (request, response) {
   response.sendStatus(200);
 });
 
+// Stop video player
+app.get("/stop", function (request, response) {
+  console.log("Stop request received");
+  kodi.Player.Stop({playerid:1});
+  response.sendStatus(200);
+});
 
+// Parse request to watch a movie
+// Request format:   http://[THIS_SERVER_IP_ADDRESS]/playmovie?q[MOVIE_NAME]
 app.get("/playmovie", function (request, response) {
   if (request == null || request.query == null) {
     response.sendStatus(200);
@@ -38,17 +58,16 @@ app.get("/playmovie", function (request, response) {
     if(!(movies && movies.result && movies.result.movies && movies.result.movies.length > 0)) {
       throw new Error('no results');
     }
- 
-    console.log("Movies list: ");
-    var movie = movies.result.movies.reduce(function(result, item) {
-      process.stdout.write("\"" + item.label.toLowerCase() + "\",  ");
-      return result ? result : (movieTitle.toLowerCase() === item.label.toLowerCase() ? item : null);
-    }, null);
-    console.log(" "); //newline
- 
-    if(movie) {
-      console.log("Found movie \"" + movie.label + "\" (" + movie.movieid + ")");
-      return kodi.Player.Open({item: { movieid: movie.movieid }});
+
+    // Create the fuzzy search object
+    var fuse = new Fuse(movies.result.movies, fuzzySearchOptions)
+    var searchResult = fuse.search(movieTitle)
+
+    // If there's a result
+    if (searchResult.length > 0) {
+      var movieFound = searchResult[0];
+      console.log("Found movie \"" + movieFound.label + "\" (" + movieFound.movieid + ")");
+      return kodi.Player.Open({item: { movieid: movieFound.movieid }});
     } else {
       throw new Error("Couldn\'t find movie \"" + movieTitle + "\"");
     }
@@ -60,92 +79,91 @@ app.get("/playmovie", function (request, response) {
 }
 });
 
-
-
+// Parse request to watch your next unwatched episode for a given tv show
+// Request format:   http://[THIS_SERVER_IP_ADDRESS]/playtvshow?q[TV_SHOW_NAME]
 app.get("/playtvshow", function (request, response) {
-  //requestedTvShowId = null;
   if (request == null || request.query == null) {
     response.sendStatus(200);
-  } else {
-    var tvshowTitle = request.query.q;
-    tvshowTitle = tvshowTitle.trim().toLowerCase();
-    console.log("TV Show request received to play \"" + tvshowTitle + "\"");
-    
-    kodi.VideoLibrary.GetTVShows()
-    .then(function(shows) {
-    if(!(shows && shows.result && shows.result.tvshows && shows.result.tvshows.length > 0)) {
-      throw new Error('no results');
-    }
- 
-    console.log("TV Shows list: ");
-    var seriesResult = shows.result.tvshows.reduce(function(result, item) {
-      process.stdout.write("\"" + item.label.toLowerCase() + "\",  ");
-      return result ? result : (tvshowTitle === item.label.toLowerCase() ? item : null);
-    }, null);
-      console.log(" "); // newline
-      
-      if (seriesResult) {
-        var requestedTvShowId = seriesResult.tvshowid;
-        console.log("Found tv show " + seriesResult.label + " (id:" + seriesResult.tvshowid + ")");
-        return requestedTvShowId;
-      } else {
-        throw new Error("Couldn't find tv show \"" + tvshowTitle + "\"");
+    return;
+  }
+  var tvshowTitle = request.query.q;
+  tvshowTitle = tvshowTitle.trim().toLowerCase();
+  console.log("TV Show request received to play \"" + tvshowTitle + "\"");
 
+  kodi.VideoLibrary.GetTVShows()
+  .then(
+    function(shows) {
+      if(!(shows && shows.result && shows.result.tvshows && shows.result.tvshows.length > 0)) {
+        throw new Error('no results');
       }
-    })
+      // Create the fuzzy search object
+      var fuse = new Fuse(shows.result.tvshows, fuzzySearchOptions)
+      var searchResult = fuse.search(tvshowTitle)
+
+      // If there's a result
+      if (searchResult.length > 0) {
+        var tvshowFound = searchResult[0];
+        console.log("Found tv show \"" + tvshowFound.label + "\" (" + tvshowFound.tvshowid + ")");
+        return tvshowFound.tvshowid;
+      } else {
+        throw new Error("Couldn\'t find tv show \"" + tvshowTitle + "\"");
+      }
+    }
+  )
   .catch(function(e) {
     console.log(e);
-  }).then(function(requestedTvShowId) {
+  })
+  .then(
+    function(requestedTvShowId) {
       if (requestedTvShowId == null) {
         return;
       }
-  console.log("0 a looking for episdoes for " + requestedTvShowId + "...");
-    var param = {
-                  tvshowid: requestedTvShowId,
-                  properties: ['playcount', 'showtitle', 'season', 'episode'],
-                  // Sort the result so we can grab the first unwatched episode
-                  sort: {
-                    order: 'ascending',
-                    method: 'episode',
-                    ignorearticle: true
-                  }
-                }
-  console.log("0 c looking for episdoes...");
-   kodi.VideoLibrary.GetEpisodes(param)
-        .then(function (episodeResult) {
-         console.log("2 looking for episdoes..." + episodeResult.episodes);
-       if(!(episodeResult && episodeResult.result && episodeResult.result.episodes && episodeResult.result.episodes.length > 0)) {
-          throw new Error('no results');
-      }
-     var episodes = episodeResult.result.episodes;
-          // Check if there are episodes for this TV show
-          if (episodes) {
-            console.log("found episodes..");
-            // Check whether we have seen this episode already
-            var firstUnplayedEpisode = episodes.filter(function (item) {
-              return item.playcount === 0
-            })
-            if (firstUnplayedEpisode.length > 0) {
-              var episdoeToPlay = firstUnplayedEpisode[0]; // Resolve the first unplayed episode
-              console.log("Found episode " + episdoeToPlay.episodeid);
+      console.log("Searching for next episode of Show ID " + requestedTvShowId + "...");          
 
-              var param = {
-                  item: {
-                    episodeid: episdoeToPlay.episodeid
-                  }
-                }
-              return kodi.Player.Open(param);
+      // Build filter to search unwatched episodes
+      var param = {
+              tvshowid: requestedTvShowId,
+              properties: ['playcount', 'showtitle', 'season', 'episode'],
+              // Sort the result so we can grab the first unwatched episode
+              sort: {
+                order: 'ascending',
+                method: 'episode',
+                ignorearticle: true
+              }
             }
+      kodi.VideoLibrary.GetEpisodes(param)
+      .then(function (episodeResult) {
+        if(!(episodeResult && episodeResult.result && episodeResult.result.episodes && episodeResult.result.episodes.length > 0)) {
+          throw new Error('no results');
+        }
+        var episodes = episodeResult.result.episodes;
+        // Check if there are episodes for this TV show
+        if (episodes) {
+          console.log("found episodes..");
+          // Check whether we have seen this episode already
+          var firstUnplayedEpisode = episodes.filter(function (item) {
+            return item.playcount === 0
+          })
+          if (firstUnplayedEpisode.length > 0) {
+            var episdoeToPlay = firstUnplayedEpisode[0]; // Resolve the first unplayed episode
+            console.log("Playing season " + episdoeToPlay.season + " episode " + episdoeToPlay.episode + " (ID: " + episdoeToPlay.episodeid + ")");
+
+            var param = {
+                item: {
+                  episodeid: episdoeToPlay.episodeid
+                }
+              }
+            return kodi.Player.Open(param);
           }
-    })
-  .catch(function(e) {
-    console.log(e);
-  });     
-  })   
+        }
+      })
+      .catch(function(e) {
+        console.log(e);
+      });     
+    })  
   response.sendStatus(200);
 }
-});
-
+);
 
 
 
