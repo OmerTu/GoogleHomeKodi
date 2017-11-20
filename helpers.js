01a97ce5734484db264e49a6dd5215ee083dbf34
damianodonoghue <damianodonoghue@outlook.com>
2017-11-20 03:53:32 +0000

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -19,19 +19,519 @@ exports.kodiPlayPause = (request, response) => { // eslint-disable-line no-unuse
     console.log('Play/Pause request received');
     let Kodi = request.kodi;
 
-    Kodi.Player.PlayPause({ // eslint-disable-line new-cap
-        playerid: 1
+    //Audio Player
+    Kodi.Player.PlayPause({playerid: 0});
+  
+    //Video Player
+    Kodi.Player.PlayPause({playerid: 1});
+  
+    response.sendStatus(200);
+};
+
+//New Damian
+//*****************************Start of Damians Functions
+
+//Navigation Controls
+
+//Navigation Down
+exports.kodiNavDown = (request, response) => { // eslint-disable-line no-unused-vars
+    console.log('Navigate down request received');
+    let Kodi = request.kodi;
+  
+    var navDown = request.query.q.trim();
+  
+    //Loop for each button press!
+    var i=0;
+    for (i = 0; i < parseInt(navDown); i++) 
+    { 
+      Kodi.Input.Down();
+    } 
+  
+    response.sendStatus(200);
+};
+
+//Navigation Up
+exports.kodiNavUp = (request, response) => { // eslint-disable-line no-unused-vars
+    console.log('Navigate up request received');
+    let Kodi = request.kodi;
+  
+    var navUp = request.query.q.trim();
+  
+    //Loop for each button press!
+    var i=0;
+    for (i = 0; i < parseInt(navUp); i++) 
+    { 
+      Kodi.Input.Up();
+    } 
+  
+    response.sendStatus(200);
+};
+
+//Navigation Left
+exports.kodiNavLeft = (request, response) => { // eslint-disable-line no-unused-vars
+    console.log('Navigate left request received');
+    let Kodi = request.kodi;
+  
+    var navLeft = request.query.q.trim();
+  
+    //Loop for each button press!
+    var i=0;
+    for (i = 0; i < parseInt(navLeft); i++) 
+    { 
+      Kodi.Input.Left();
+    } 
+  
+    response.sendStatus(200);
+};
+
+//Navigation Right
+exports.kodiNavRight = (request, response) => { // eslint-disable-line no-unused-vars
+    console.log('Navigate right request received');
+    let Kodi = request.kodi;
+  
+    var navRight = request.query.q.trim();
+  
+    //Loop for each button press!
+    var i=0;
+    for (i = 0; i < parseInt(navRight); i++) 
+    { 
+      Kodi.Input.Right();
+    } 
+  
+    response.sendStatus(200);
+};
+
+//Navigation Back
+exports.kodiNavBack = (request, response) => { // eslint-disable-line no-unused-vars
+    console.log('Navigate back request received');
+    let Kodi = request.kodi;
+  
+    var navBack = request.query.q.trim();
+  
+    //Loop for each button press!
+    var i=0;
+    for (i = 0; i < parseInt(navBack); i++) 
+    { 
+      Kodi.Input.Back();
+    } 
+  
+    response.sendStatus(200);
+};
+
+//Navigation Select
+exports.kodiNavSelect = (request, response) => { // eslint-disable-line no-unused-vars
+    console.log('Navigation select request received');
+    let Kodi = request.kodi;
+  
+    Kodi.Input.Select();
+    response.sendStatus(200);
+};
+
+//Navigation ContextMenu
+exports.kodiNavContextMenu = (request, response) => { // eslint-disable-line no-unused-vars
+    console.log('Navigation ContextMenu request received');
+    let Kodi = request.kodi;
+  
+    Kodi.Input.ContextMenu();
+    response.sendStatus(200);
+};
+
+// Show Info
+exports.kodiDisplayInfo = (request, response) => { // eslint-disable-line no-unused-vars
+    console.log('Display information request received');
+    let Kodi = request.kodi;
+  
+    Kodi.Input.Info();
+    response.sendStatus(200);
+};
+
+//Navigation Home
+exports.kodiNavHome = (request, response) => { // eslint-disable-line no-unused-vars
+    console.log('Navigation Home request received');
+    let Kodi = request.kodi;
+  
+    Kodi.Input.Home();
+    response.sendStatus(200);
+};
+
+// Set subtitles
+exports.kodiSetSubs = (request, response) => { // eslint-disable-line no-unused-vars
+    let Kodi = request.kodi;
+  
+    var setsubs = request.query.q.trim();
+    console.log('Change subtitles request received - ' + setsubs);
+
+    if(setsubs == "previous" || setsubs == "next" || setsubs == "on" || setsubs == "off")
+    {      
+      Kodi.Player.SetSubtitle({"playerid":1,"subtitle":setsubs});
+    }
+    else
+    {
+      //Else parse the number for the subtitles track to set.
+      var subtitleIndex = parseInt(setsubs);      
+
+      Kodi.Player.SetSubtitle({"playerid":1,"subtitle":subtitleIndex});
+    }
+    
+    response.sendStatus(200);
+};
+
+// Set audiostream
+exports.kodiSetAudio = (request, response) => { // eslint-disable-line no-unused-vars
+    let Kodi = request.kodi;
+  
+    var setaudiostream = request.query.q.trim();
+    
+    if(setaudiostream == "previous" || setaudiostream == "next")
+    {      
+      Kodi.Player.SetAudioStream({"playerid":1,"stream":setaudiostream});
+      
+      console.log('Change audio stream request received - ' + setaudiostream);
+    }
+    else
+    {
+      //If not previous or next then set the audio stream to an index
+      var audioStreamIndex;
+      
+      if(setaudiostream == "zero")
+      {
+        audioStreamIndex = 0
+      }
+      if(setaudiostream == "one")
+      {
+        audioStreamIndex = 1
+      }
+      else if(setaudiostream == "to" || setaudiostream == "too" || setaudiostream == "two")
+      {
+        audioStreamIndex = 2
+      }
+      else if(setaudiostream == "three")
+      {
+        audioStreamIndex = 3
+      }
+      else if(setaudiostream == "four")
+      {
+        audioStreamIndex = 4
+      }
+      else if(setaudiostream == "five")
+      {
+        audioStreamIndex = 5
+      }
+      else if(setaudiostream == "six")
+      {
+        audioStreamIndex = 6
+      }
+      else if(setaudiostream == "seven")
+      {
+        audioStreamIndex = 7
+      }
+      else if(setaudiostream == "eight")
+      {
+        audioStreamIndex = 8
+      }
+      else if(setaudiostream == "nine")
+      {
+        audioStreamIndex = 9
+      }
+      else if(setaudiostream == "ten")
+      {
+        audioStreamIndex = 10
+      }
+      else
+      {
+        //Sometimes the num is detected instead of the spelt number. if this is the case it can just be parsed!
+        //The number for the audio stream to set.
+        audioStreamIndex = parseInt(setaudiostream);   
+        console.log('Parsed Int ' + audioStreamIndex);
+      }
+   
+      Kodi.Player.SetAudioStream({"playerid":1,"stream":audioStreamIndex});
+      
+      //Write to log
+      console.log('Change audio stream request received - Index ' + audioStreamIndex);
+    }
+  
+    response.sendStatus(200);
+};
+
+// Go to x minutes
+exports.kodiSeektominutes = (request, response) => { // eslint-disable-line no-unused-vars
+    console.log('Skip to x minutes request received');
+    let Kodi = request.kodi;
+  
+    var seektominutes = request.query.q.trim();
+  
+    //Audio player
+    Kodi.Player.Seek({"playerid":0,"value":{"minutes":parseInt(seektominutes)} });
+  
+    //Video player
+    Kodi.Player.Seek({"playerid":1,"value":{"minutes":parseInt(seektominutes)} });
+  
+    response.sendStatus(200);
+};
+
+/*
+//Bug when seeking forward less than 60 seconds in kodi json https://forum.kodi.tv/showthread.php?tid=237408 so I'm disabling this until a work around is working.
+// Seek x seconds forward
+exports.kodiSeekForwardSeconds = (request, response) => { // eslint-disable-line no-unused-vars
+    console.log('Seek x minutes forwards request received V2');
+    let Kodi = request.kodi;
+  
+    var seekForwardSeconds = request.query.q.trim();
+  
+    //Audio Player
+    Kodi.Player.Seek({"playerid":0,"value":{"seconds":parseInt(seekForwardSeconds)} });
+  
+    //Video Player
+    //If its under a minute it goes to instead of skipping
+    Kodi.Player.Seek({"playerid":1,"value":{"seconds":parseInt(seekForwardSeconds)} });
+  
+    response.sendStatus(200);
+};
+
+// Seek x seconds backward
+exports.kodiSeekBackwardSeconds = (request, response) => { // eslint-disable-line no-unused-vars
+    console.log('Seek x seconds backward request received');
+    let Kodi = request.kodi;
+  
+    var seekbackwardSeconds = request.query.q.trim();
+
+    //Audio Player  
+    //Kodi.Player.Seek({"playerid":0,"value":{"seconds":parseInt(-seekbackwardSeconds)} });
+  
+    //Video Player
+    Kodi.Player.Seek({"playerid":1,"value":{"seconds":parseInt(-seekbackwardSeconds)} });
+  
+    response.sendStatus(200);
+};
+*/
+
+
+// Seek x minutes forwards
+exports.kodiSeekForwardMinutes = (request, response) => { // eslint-disable-line no-unused-vars
+    console.log('Seek x minutes forwards request received');
+    let Kodi = request.kodi;
+  
+    var seekForwardminutes = request.query.q.trim();
+  
+    //Audio Player
+    Kodi.Player.Seek({"playerid":0,"value":{"seconds":parseInt(seekForwardminutes*60)} });
+  
+    //Video Player
+    Kodi.Player.Seek({"playerid":1,"value":{"seconds":parseInt(seekForwardminutes*60)} });
+  
+    response.sendStatus(200);
+};
+
+// Seek x minutes backwards
+exports.kodiSeekBackwardMinutes = (request, response) => { // eslint-disable-line no-unused-vars
+    console.log('Seek x minutes backward request received');
+    let Kodi = request.kodi;
+  
+    var seekbackwardminutes = request.query.q.trim();
+  
+    //Audio Player
+    Kodi.Player.Seek({"playerid":0,"value":{"seconds":parseInt(-seekbackwardminutes*60)} });
+  
+    //Video Player
+    Kodi.Player.Seek({"playerid":1,"value":{"seconds":parseInt(-seekbackwardminutes*60)} });
+  
+    response.sendStatus(200);
+};
+
+//Play Song
+exports.kodiPlaySong = (request, response) => { // eslint-disable-line no-unused-vars
+    tryActivateTv(request, response);
+
+    let songTitle = request.query.q.trim();
+    let Kodi = request.kodi;
+
+    console.log(`Song request received to play "${songTitle}"`);
+    kodiFindSong(songTitle, Kodi).then((data) => {
+        return Kodi.Player.Open({ // eslint-disable-line new-cap
+            item: {
+                songid: data.songid
+            }
+        });
+    }).catch((error) => {
+        console.log(error);
     });
     response.sendStatus(200);
 };
+
+const kodiFindSong = (songTitle, Kodi) => {
+    return new Promise((resolve, reject) => {
+        Kodi.AudioLibrary.GetSongs() // eslint-disable-line new-cap
+        .then((songs) => {
+            if (!(songs && songs.result && songs.result.songs && songs.result.songs.length > 0)) {
+                throw new Error('no results');
+            }
+
+            // Create the fuzzy search object
+            let fuse = new Fuse(songs.result.songs, fuzzySearchOptions);
+            let searchResult = fuse.search(songTitle);
+
+            // If there's a result
+            if (searchResult.length > 0) {
+                let songFound = searchResult[0];
+
+                console.log(`Found song "${songFound.label}" (${songFound.songid})`);
+                resolve(songFound);
+            } else {
+                reject(`Couldn't find song "${songTitle}"`);
+            }
+        })
+        .catch((e) => {
+            reject(e);
+        });
+    });
+};
+
+//Play Artist
+exports.kodiPlayArtist = (request, response) => { // eslint-disable-line no-unused-vars
+    tryActivateTv(request, response);
+
+    let artistTitle = request.query.q.trim();
+    let Kodi = request.kodi;
+
+    console.log(`Artist request received to play "${artistTitle}"`);
+    kodiFindArtist(artistTitle, Kodi).then((data) => {
+        return Kodi.Player.Open({ // eslint-disable-line new-cap
+            item: {
+                artistid: data.artistid
+            }
+        });
+    }).catch((error) => {
+        console.log(error);
+    });
+    response.sendStatus(200);
+};
+
+const kodiFindArtist = (artistTitle, Kodi) => {
+    return new Promise((resolve, reject) => {
+        Kodi.AudioLibrary.GetArtists() // eslint-disable-line new-cap
+        .then((artists) => {
+            if (!(artists && artists.result && artists.result.artists && artists.result.artists.length > 0)) {
+                throw new Error('no results');
+            }
+
+            // Create the fuzzy search object
+            let fuse = new Fuse(artists.result.artists, fuzzySearchOptions);
+            let searchResult = fuse.search(artistTitle);
+
+            // If there's a result
+            if (searchResult.length > 0) {
+                let artistFound = searchResult[0];
+
+                console.log(`Found artist "${artistFound.label}" (${artistFound.artistid})`);
+                resolve(artistFound);
+            } else {
+                reject(`Couldn't find artist "${artistTitle}"`);
+            }
+        })
+        .catch((e) => {
+            reject(e);
+        });
+    });
+};
+
+//Play Album
+exports.kodiPlayAlbum = (request, response) => { // eslint-disable-line no-unused-vars
+    tryActivateTv(request, response);
+
+    let albumTitle = request.query.q.trim();
+    let Kodi = request.kodi;
+
+    console.log(`Album request received to play "${albumTitle}"`);
+    kodiFindAlbum(albumTitle, Kodi).then((data) => {
+        return Kodi.Player.Open({ // eslint-disable-line new-cap
+            item: {
+                albumid: data.albumid
+            }
+        });
+    }).catch((error) => {
+        console.log(error);
+    });
+    response.sendStatus(200);
+};
+
+const kodiFindAlbum = (albumTitle, Kodi) => {
+    return new Promise((resolve, reject) => {
+        Kodi.AudioLibrary.GetAlbums() // eslint-disable-line new-cap
+        .then((albums) => {
+            if (!(albums && albums.result && albums.result.albums && albums.result.albums.length > 0)) {
+                throw new Error('no results');
+            }
+
+            // Create the fuzzy search object
+            let fuse = new Fuse(albums.result.albums, fuzzySearchOptions);
+            let searchResult = fuse.search(albumTitle);
+
+            // If there's a result
+            if (searchResult.length > 0) {
+                let albumFound = searchResult[0];
+
+                console.log(`Found album "${albumFound.label}" (${albumFound.albumid})`);
+                resolve(albumFound);
+            } else {
+                reject(`Couldn't find album "${albumTitle}"`);
+            }
+        })
+        .catch((e) => {
+            reject(e);
+        });
+    });
+};
+
+// Player Control
+exports.playercontrol = (request, response) => { // eslint-disable-line no-unused-vars
+    let Kodi = request.kodi;
+  
+    var playercommand = request.query.q.trim();
+    console.log(`Player control request received - "${playercommand}"`);
+  
+  //Previous not working correctly! It just resets the song to 00:00 another kodi json bug!
+  if(playercommand == "previous")
+    {      
+      //Audio Player
+      Kodi.Player.GoTo({"playerid":0,"to":playercommand});
+      
+      //Video Player
+      Kodi.Player.GoTo({"playerid":1,"to":playercommand});
+    }
+  else if(playercommand == "next")
+    {
+      //Audio Player
+      Kodi.Player.GoTo({"playerid":0,"to":playercommand});
+      
+     //Video Player
+     Kodi.Player.GoTo({"playerid":1,"to":playercommand});
+    }
+  else
+    {
+      var playlistindex = parseInt(playercommand) -1;
+      
+      //Audio Player
+      Kodi.Player.GoTo({"playerid":0,"to":playlistindex});
+      
+      //Video Player
+      Kodi.Player.GoTo({"playerid":1,"to":playlistindex});
+    }
+    
+    response.sendStatus(200);
+};
+//*****************************End of Damians Functions
 
 exports.kodiStop = (request, response) => { // eslint-disable-line no-unused-vars
     console.log('Stop request received');
     let Kodi = request.kodi;
 
-    Kodi.Player.Stop({ // eslint-disable-line new-cap
-        playerid: 1
-    });
+    //Audio Player
+    Kodi.Player.Stop({playerid: 0});
+  
+    //Video Player
+    Kodi.Player.Stop({playerid: 1});
+    
     response.sendStatus(200);
 };
 
@@ -487,16 +987,6 @@ exports.kodiPlayYoutube = (request, response) => { // eslint-disable-line no-unu
                     file: `plugin://plugin.video.youtube/?action=play_video&videoid=${results[0].id}`
                 }
             });
-        }
-    });
-};
-
-exports.kodiSeek = (request, response) => { // eslint-disable-line no-unused-vars
-    let seekForward = request.query.q.trim();
-
-    kodi.Player.Seek({ // eslint-disable-line new-cap
-        'playerid': 1, 'value': {
-            'seconds': parseInt(seekForward)
         }
     });
 };
