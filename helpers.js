724adf4bc42f595f02de374858906c7223a0f2f2
janvitos <10037804+janvitos@users.noreply.github.com>
2018-08-04 23:07:58 -0400

    added kodiToggleFullscreen helper function

--- helpers.js
+++ helpers.js
@@ -1118,3 +1118,11 @@ exports.kodiTogglePartymode = (request) => {
         .catch(() => AUDIO_PLAYER)
         .then((playerid) => togglePartyMode(kodi, playerid));
 };
+
+exports.kodiToggleFullscreen = (request) => { // eslint-disable-line no-unused-vars
+    console.log('Toggle Fullscreen request received');
+    let Kodi = request.kodi;
+    return Kodi.Input.ExecuteAction({
+		'action': 'togglefullscreen'
+	});
+};
